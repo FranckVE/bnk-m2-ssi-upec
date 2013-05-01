b10e8d6130ed38096cda34bd1c7ef3a9b2fecfb1
Abdellah AZOUGARH <abdellah.azougarh@gmail.com>
2013-05-01 21:34:13 +0200

    Ajout d'applet

--- Banque/src/sdcard/Test.java
+++ Banque/src/sdcard/Test.java
@@ -1,16 +1,20 @@
 package sdcard;
 
 import java.applet.Applet;
+import java.awt.HeadlessException;
 
-public class Test extends Applet{
+public class Test extends Applet {
+
+	public Test() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		  Login login = new Login() ;
-		  login.setVisible(true);
+
 	}
 
 }
